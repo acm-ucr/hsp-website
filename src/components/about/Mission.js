22a41ecb5c7a33ee3eb5cad2b50c2771bea631ae
Vaneesha Singh <vsingh@Vaneeshas-MacBook-Pro.local>
2024-01-17 09:44:38 -0800

    Create Mission Section

--- src/components/about/Mission.js
+++ src/components/about/Mission.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Image from "next/image";
 
-const mission = () => {
-  return <div>mission</div>;
+const Mission = (props) => {
+  return (
+    <div className="my-12">
+      <div className="text-white text-center text-2xl font-black mb-10">
+        {props.title}
+      </div>
+
+      <div className="ml-16">
+        <Image src={props.img} alt="hsp building and testing rockets" />
+      </div>
+
+      <div className="text-white text-center text-lg font-black mt-8 mx-16">
+        {props.text}
+      </div>
+
+      <div className="text-white text-center text-lg font-black mt-4 mx-16">
+        {props.para}
+      </div>
+    </div>
+  );
 };
 
-export default mission;
+export default Mission;
