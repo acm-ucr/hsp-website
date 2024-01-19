50123aa718b0b726384d19526296c7800801c6f3
kavinphab <kavinphab@gmail.com>
2024-01-19 14:20:34 -0800

    started card

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Profile from "../components/board/Profile";
 const Home = () => {
-  return <>Hello World</>;
+  return (
+    <Profile
+      name={"Javen Dosanjh"}
+      title={"Chief Engineer & President of Operations"}
+      year={"4th year"}
+      major={"Mechanical Engineering"}
+    />
+  );
 };
 
 export default Home;
--- src/components/board/Profile.js
+++ src/components/board/Profile.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import { MdEmail } from "react-icons/md";
+import { GrLinkedinOption } from "react-icons/gr";
 
-const profile = () => {
-  return <div>profile</div>;
+const Profile = ({
+  name,
+  image,
+  title,
+  email,
+  linkedin,
+  year,
+  major,
+  pronouns,
+}) => {
+  return (
+    <div className="border-2 border-black font-poppins bg-white w-64 h-96 m-5">
+      <Image
+        src={image}
+        alt="Picture of board member"
+        className="rounded-full border-2 border-black mt-3 mr-3 ml-3"
+      />
+      <div className="ml-3">
+        <div className="font-bold text-xl mt-3">{name}</div>
+        <div className="text-l">{title}</div>
+        <div className="flex space-x-2 text-slate-700">
+          <MdEmail className="h-8 w-8 " />
+          <GrLinkedinOption className="h-7 w-7" />
+        </div>
+        <div className="text-l">
+          {year}
+
+          {major}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default profile;
+export default Profile;
