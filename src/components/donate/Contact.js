f3c16290a6ed728faf3176d0c7474f48ca33cb52
Arnav Swami <arnavswami@Arnavs-MacBook-Air-2.local>
2024-01-18 23:17:16 -0800

    Created contact page

--- src/components/donate/Contact.js
+++ src/components/donate/Contact.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <p className="text-7xl">Contact Us</p>
+
+      <p className="mb-14"> highlanderspaceprogram@gmail.com </p>
+
+      <div className="mb-5 font-bold">Javen Dosanjh</div>
+
+      <p className="mb-1">President/Chief Engineer</p>
+      <p className="mb-1">jdos001@ucr.edu</p>
+      <p className="mb-5">(916)-840-8409</p>
+
+      <div className="mb-5 font-bold">Jordy Hendra</div>
+
+      <p className="mb-1">Vice President</p>
+      <p className="mb-5">jhend060@ucr.edu</p>
+
+      <p className="mb-1">Winston Chung Hall, 446</p>
+
+      <p className="mb-1">N Campus Dr, Riverside</p>
+
+      <p className="mb-1">CA 92507</p>
+    </div>
+  );
 };
 
 export default Contact;
