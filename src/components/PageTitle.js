956a62e4aa8ec1a8737238869e48c819bdb320ee
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-01-19 17:47:03 -0800

    created page title

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import starBackground from "@/public/assets/starBackground.svg";
 
-const PageTitle = () => {
-  return <div>PageTitle</div>;
+const PageTitle = ({ PageTitle }) => {
+  return (
+    <div className="page-title-container">
+      <div className="relative page-title">
+        <Image src={starBackground} alt="Star Background" className="w-full" />
+        <div class="font-mont absolute top-12 py-3.5 inset-x-0 text-white text-7xl font-bold text-center">
+          {PageTitle}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageTitle;
