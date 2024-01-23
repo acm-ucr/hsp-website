066959cb31f2e2eb348e7378b2fb0b125dcad76f
Raidah Fairooz <rfair008@ucr.edu>
2024-01-22 23:25:51 -0800

    data added

--- src/components/about/Us.js
+++ src/components/about/Us.js
@@ -9,35 +9,32 @@ const Us = () => {
     <div className="text-center flex flex-col justify-center items-center">
       <div className="my-40 max-h-0 translate-y-12 flex flex-col1">
         <GrBottomCorner
-          size={50}
+          size={65}
           className="origin-top-left translate-x-7 translate-y-5 rotate-90"
         />
-        <h1 className="font-montserrat ">
-          {" "}
-          <strong> ABOUT US</strong>
-        </h1>
+        <div className="font-montserrat font-bold text-5xl">ABOUT US</div>
         <GrTopCorner
-          size={50}
+          size={65}
           className="origin-top-right translate-x-5 translate-y-8 rotate-90"
         />
       </div>
-      <div className="grid grid-cols-2 justify-center w-8/12">
-        <div className="flex flex-col text-center justify-center items-center font-montserrat">
-          <b className="text-lg w-11/12">
+      <div className="grid grid-cols-2 justify-center w-full gap-4">
+        <div className="flex flex-col text-center justify-center items-end font-montserrat font-bold ">
+          <div className="text-lg w-9/12">
             {" "}
             HSP creates opportunities for its members to collaborate with
             industry professionals and organizations, engage in projects and
             research that &nbsp; are relevant to the field of aerospace
             engineering.
-          </b>
+          </div>
           <br />
-          <b className="text-lg w-11/12">
+          <div className="text-lg w-9/12">
             HSP regularly conducts meetings and work sessions that build towards
             competitions.
-          </b>
+          </div>
         </div>
-        <div className="flex justify-center items-center w-11/12">
-          <Image width={500} alt="" src={image} />
+        <div className="flex justify-start items-center ">
+          <Image alt="" src={image} />
         </div>
       </div>
     </div>
