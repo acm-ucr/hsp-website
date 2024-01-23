59a04c9a56520e2d1b397f8577e7af5c7588c444
Raidah Fairooz <rfair008@ucr.edu>
2024-01-23 00:27:28 -0800

    added board assets

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Profile from "../components/board/Profile";
+import Javen from "../public/assets/board/Javen.svg";
 const Home = () => {
   return (
     <Profile
       name={"Javen Dosanjh"}
+      image={Javen}
       title={"Chief Engineer & President of Operations"}
       year={"4th year"}
       major={"Mechanical Engineering"}
