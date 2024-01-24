ec8b181fc202cc751418fbc20b8a0bd2c20ccc87
kavinphab <kavinphab@gmail.com>
2024-01-24 11:30:14 -0800

    formatting

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,7 @@
 import React from "react";
-import Profile from "../components/board/Profile";
-import Javen from "../public/assets/board/Javen.svg";
 
 const Home = () => {
-  return (
-    <Profile
-      name={"Javen Dosanjh"}
-      title={"Chief Engineer & President of Operations"}
-      year={"4th year"}
-      major={"Mechanical Engineering"}
-      pronouns={"He/Him"}
-      image={Javen}
-      email={"mailto:asdf"}
-      linkedin={"https://www.linkedin.com/in/javen-dosanjh-5a3a001a4/"}
-
-    />
-  );
+  return <>Home</>;
 };
 
 export default Home;
