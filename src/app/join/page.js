7d9efd2b6b1870ab1854011661ad6421e0eaa742
kelly <kelly@ucr-secure-32-10-13-178-133.wnet.ucr.edu>
2024-01-25 15:28:34 -0800

    created join card

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Card from "@/components/join/Card";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="justify justify-center grid grid-cols-3 mx-28 my-28">
+      <Card
+        title="Come to our meetings and events"
+        description="HSP regularly conducts meetings and work sessions that build towards competitions."
+      />
+      <Card
+        title="Join Our Discord"
+        description="Join our Discord to communicate with us and have access to all our channels. It’s a great resource for seeking internships and on-campus jobs."
+      />
+      <Card
+        title="Follow our Socials"
+        description="Following our socials will keep you updated on what's happening with HSP!"
+      />
+    </div>
+  );
 };
 
 export default page;
--- src/components/join/Card.js
+++ src/components/join/Card.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  IoMdCloseCircleOutline,
+  IoMdRemoveCircleOutline,
+} from "react-icons/io";
+// import { MdGroups } from "react-icons/md";
+import { FaDiscord } from "react-icons/fa";
+// import { FaBell } from "react-icons/fa";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ title, description }) => {
+  return (
+    <div className="bg-white text-black w-96 h-96 border border-black rounded-3xl">
+      <div className="flex items-center text-2xl mx-4 my-2">
+        <IoMdCloseCircleOutline className="mx-1" />
+        <IoMdRemoveCircleOutline className="mx-1" />
+      </div>
+      <div className="border border-black"></div>
+      <div className="font-montserrat text-3xl font-extrabold leading-10 p-3">
+        {title}
+      </div>
+      <div className="font-sans-arial text-xl font-light leading-9 px-3">
+        {description}
+      </div>
+      <div className="flex justify justify-center text-6xl pt-3">
+        <FaDiscord />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
