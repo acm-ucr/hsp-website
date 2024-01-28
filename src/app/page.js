99b9e408516cccc6fbe234631710fa343726ec0c
kavinphab <kavinphab@gmail.com>
2024-01-28 11:34:06 -0800

    fixed spacing for cards and added executive title

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PageTitle from "@/components/PageTitle";
+import Team from "@/components/board/Team";
 
 const Home = () => {
   return (
     <>
       <PageTitle PageTitle="Title" />
+      <div>
+        <Team />
+      </div>
     </>
   );
+};
 export default Home;
--- src/components/board/Team.js
+++ src/components/board/Team.js
@@ -3,55 +3,63 @@ import Profile from "@/components/board/Profile";
 import Javen from "@/public/assets/board/Javen.svg";
 const Team = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="grid grid-cols-4 w-10/12  gap-3 justify-center items-center bg-red-200">
-        <div>
-          <Profile
-            name="Javen Dosanjh"
-            email="rfair008@ucr.edu"
-            title="Vice President"
-            image={Javen}
-            year="2nd"
-            major="Computer Science"
-            pronouns="he/him"
-            linkedin="linkedin.com"
-          />
+    <div>
+      <div className="font-montserrat flex justify-center text-4xl font-light ">
+        <div className="w-1/4 border-b border-black flex justify-center h-14">
+          executive leads
         </div>
-        <div>
-          <Profile
-            name="Javen Dosanjh"
-            email="rfair008@ucr.edu"
-            title="Vice President"
-            image={Javen}
-            year="2nd"
-            major="Computer Science"
-            pronouns="he/him"
-            linkedin="linkedin.com"
-          />
-        </div>
-        <div>
-          <Profile
-            name="Javen Dosanjh"
-            email="rfair008@ucr.edu"
-            title="Vice President"
-            image={Javen}
-            year="2nd"
-            major="Computer Science"
-            pronouns="he/him"
-            linkedin="linkedin.com"
-          />
-        </div>
-        <div>
-          <Profile
-            name="Javen Dosanjh"
-            email="rfair008@ucr.edu"
-            title="Vice President"
-            image={Javen}
-            year="2nd"
-            major="Computer Science"
-            pronouns="he/him"
-            linkedin="linkedin.com"
-          />
+      </div>
+      <br />
+      <div className="flex justify-center items-center bg-green-200">
+        <div className="grid grid-cols-4 gap-7 justify-center items-center bg-red-200">
+          <div>
+            <Profile
+              name="Javen Dosanjh"
+              email="rfair008@ucr.edu"
+              title="Vice President"
+              image={Javen}
+              year="2nd"
+              major="Computer Science"
+              pronouns="he/him"
+              linkedin="linkedin.com"
+            />
+          </div>
+          <div>
+            <Profile
+              name="Javen Dosanjh"
+              email="rfair008@ucr.edu"
+              title="Vice President"
+              image={Javen}
+              year="2nd"
+              major="Computer Science"
+              pronouns="he/him"
+              linkedin="linkedin.com"
+            />
+          </div>
+          <div>
+            <Profile
+              name="Javen Dosanjh"
+              email="rfair008@ucr.edu"
+              title="Vice President"
+              image={Javen}
+              year="2nd"
+              major="Computer Science"
+              pronouns="he/him"
+              linkedin="linkedin.com"
+            />
+          </div>
+          <div>
+            <Profile
+              name="Javen Dosanjh"
+              email="rfair008@ucr.edu"
+              title="Vice President"
+              image={Javen}
+              year="2nd"
+              major="Computer Science"
+              pronouns="he/him"
+              linkedin="linkedin.com"
+            />
+          </div>
         </div>
       </div>
     </div>
