7f3a76e40e64e1f2ea2472f26ebf4e127be9521d
kelly <kelly@ucr-secure-31-10-13-173-34.wnet.ucr.edu>
2024-01-28 18:09:53 -0800

    added icons

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Card from "@/components/join/Card";
+import { MdGroups } from "react-icons/md";
+import { FaDiscord } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
 
 const page = () => {
   return (
@@ -7,14 +10,17 @@ const page = () => {
       <Card
         title="Come to our meetings and events"
         description="HSP regularly conducts meetings and work sessions that build towards competitions."
+        icon={<MdGroups className="text-black" size={100} />}
       />
       <Card
         title="Join Our Discord"
         description="Join our Discord to communicate with us and have access to all our channels. It’s a great resource for seeking internships and on-campus jobs."
+        icon={<FaDiscord className="text-black pt-3" size={75} />}
       />
       <Card
         title="Follow our Socials"
         description="Following our socials will keep you updated on what's happening with HSP!"
+        icon={<FaBell className="text-black justify pt-20" size={150} />}
       />
     </div>
   );
--- src/components/join/Card.js
+++ src/components/join/Card.js
@@ -3,11 +3,8 @@ import {
   IoMdCloseCircleOutline,
   IoMdRemoveCircleOutline,
 } from "react-icons/io";
-// import { MdGroups } from "react-icons/md";
-import { FaDiscord } from "react-icons/fa";
-// import { FaBell } from "react-icons/fa";
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, icon }) => {
   return (
     <div className="bg-white text-black w-96 h-96 border border-black rounded-3xl">
       <div className="flex items-center text-2xl mx-4 my-2">
@@ -21,8 +18,8 @@ const Card = ({ title, description }) => {
       <div className="font-sans-arial text-xl font-light leading-9 px-3">
         {description}
       </div>
-      <div className="flex justify justify-center text-6xl pt-3">
-        <FaDiscord />
+      <div className="flex justify justify-center">
+        <div>{icon}</div>
       </div>
     </div>
   );
