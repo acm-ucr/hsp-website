3d78006973ed4d7bcacf553bfce7f1c11a3995b5
kavinphab <kavinphab@gmail.com>
2024-01-30 23:08:23 -0800

    finished setting up grid and spacing

--- src/components/board/Team.js
+++ src/components/board/Team.js
@@ -6,7 +6,8 @@ const Team = () => {
   return (
     <div className="flex flex-col justify-center items-center bg-green-500 w-full">
       <LeadTitle Name="executive" />
-      <div className="bg-red-200 w-full justify-center items-center grid grid-cols-4">
+      <br />
+      <div className="grid grid-cols-4 bg-red-200 w-10/12 justify-items-center">
         {BOARD[0].map((person, index) => (
           <Profile
             key={index}
@@ -22,8 +23,8 @@ const Team = () => {
         ))}
       </div>
       <LeadTitle Name="technical" />
-      <div className="bg-red-200 w-full justify-center items-center grid grid-cols-4">
-        {BOARD[0].map((person, index) => (
+      <div className="bg-red-200 w-10/12 justify-items-center grid grid-cols-4">
+        {BOARD[1].map((person, index) => (
           <Profile
             key={index}
             name={person.name}
@@ -37,7 +38,21 @@ const Team = () => {
           />
         ))}
       </div>
-      <div className="grid grid-cols-4"></div>
+      <div className="grid grid-cols-3 w-3/5 bg-red-200 justify-items-center pt-2 pb-4">
+      {BOARD[2].map((person, index) => (
+          <Profile
+            key={index}
+            name={person.name}
+            title={person.title}
+            image={person.image}
+            email={person.email}
+            linkedin={person.linkedin}
+            year={person.year}
+            major={person.major}
+            pronouns={person.pronouns}
+          />
+        ))}
+      </div>
     </div>
   );
 };
