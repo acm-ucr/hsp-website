402f3181c2e3e97f4247390899cd8f7d14a733f7
David <davidhong0901@gmail.com>
2024-02-07 20:27:49 -0800

    LeadsTitle.js changed into Header.js

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PageTitle from "@/components/PageTitle";
+import HeaderTitle from "@/components/board/Header";
+
 const page = () => {
   return (
     <div>
       <PageTitle title="EVENTS" />
+      <HeaderTitle Name={"Upcoming Events"} />
+      <HeaderTitle Name={"Past Events"} />
     </div>
   );
 };
