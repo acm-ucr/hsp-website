2020f30ad4ddf330e14bb44ddb97ad186ca003e9
Charleen Chen <charleenc485@gmail.com>
2024-02-08 13:35:56 -0800

    fixed donate button

--- src/components/donate/Donate.js
+++ src/components/donate/Donate.js
@@ -5,7 +5,7 @@ const Donate = () => {
   return (
     <Link
       href="#"
-      className="bg-hsp-green text-white text-[60px] font-urbanist font-black underline flex justify-center items-center rounded-[40.5rem] w-[364] h-[81] my-7 px-2.5 py-0 decoration-1 underline-offset-4 hover:scale-110"
+      className="bg-hsp-green text-white text-6xl font-urbanist font-black underline flex justify-center items-center rounded-[40.5rem] min-w-fit h-20 my-7 px-2.5 py-0 decoration-1 underline-offset-4 hover:scale-110"
     >
       Donate Here
     </Link>
