b1187ab22f2d9fa82cf7fd499942b27b9b9b0de2
Raidah Fairooz <rfair008@ucr.edu>
2024-02-10 19:08:55 -0800

    small fixes

--- src/components/projects/poseidon/Timeline.js
+++ src/components/projects/poseidon/Timeline.js
@@ -3,30 +3,31 @@ import texts from "@/data/Poseidon/Timeline";
 
 const Timeline = () => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-items-start w-8/12 text-4xl font-bold pb-4">
+    <div className="flex flex-col items-center my-10">
+      <div className="flex text-left w-9/12 text-3xl font-extrabold font-montserrat pb-4">
         Timeline
       </div>
       {texts.map((event, index) => (
-        <div key={index} className="flex w-2/3 items-start ">
+        <div key={index} className="flex w-9/12 items-start ">
           <div className="flex items-center justify-center">
             <div>
-              {/* Container for the circle and the line */}
               <div className="flex flex-col items-center">
-                {/* Circle */}
-                <div className=" bg-blue-500 rounded-full w-16 h-16"></div>
+                <div className=" bg-blue-500 rounded-full w-10 h-10"></div>
 
-                {/* Vertical line below the circle */}
                 {index !== texts.length - 1 && (
-                  <div className="w-1 bg-black h-32"></div>
+                  <div className="w-1 bg-black h-20"></div>
                 )}
               </div>
             </div>
           </div>
-          {/* Texts */}
-          <div className="grid grid-cols-2 gap-0.5">
-            <div className="text-4xl place-self-center">{event.data}</div>
-            <div className="text-2xl text-left">{event.para}</div>
+
+          <div className="grid grid-cols-2 font-sans ">
+            <div className="text-2xl flex justify-center items-start ">
+              {event.data}
+            </div>
+            <div className="text-xl text-left  flex justify-center items-center">
+              {event.para}
+            </div>
           </div>
         </div>
       ))}
