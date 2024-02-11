50b3bb7be7a58110132cb0572dd4f62a3471d4a1
Raidah Fairooz <rfair008@ucr.edu>
2024-02-10 21:26:20 -0800

    event card

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -13,24 +13,16 @@ const Event = ({ month, day, start, title, location, description }) => {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
   return (
-    <div className="flex justify-center h-[168.81px] my-[2rem] drop-shadow-2xl">
-      <div className="font-urbanist bg-gradient-to-r from-[#000000] to-[#575966] text-[#ffffff] rounded-l-[18px] w-[134.48px]">
-        <div className="text-[42px] mt-3 text-center md:text-5xl font-black ">
-          {month}
-        </div>
-        <div className="text-[42px] text-center md:text-5xl font-black pt-2">
-          {day}
-        </div>
-        <div className="text-[21px] text-center md:text-2xl font-black py-2">
-          {start}
-        </div>
+    <div className="flex justify-center drop-shadow-2xl w-5/12 ">
+      <div className="font-urbanist flex justify-center flex-col bg-gradient-to-r from-[#010101] via-[#010101]  to-[#393B42] text-[#ffffff] rounded-l-2xl w-3/12">
+        <div className=" mt-3 text-center md:text-5xl font-black ">{month}</div>
+        <div className="text-center md:text-4xl font-black pt-2">{day}</div>
+        <div className="text-center md:text-xl font-black pt-2">{start}</div>
       </div>
-      <div className="bg-[#ffffff] rounded-r-[18px] w-[461.31px] text-[#303030]">
-        <div className="leading-tight text-[34px] ml-5 font-black">{title}</div>
-        <div className="leading-tight font-arial ml-5 flex text-left text-[21px]">
-          {location}
-        </div>
-        <div className="leading-snug font-arial ml-5 mb-2 mt-3 mr-2 text-[18px]">
+      <div className="bg-[#ffffff] rounded-r-2xl w-9/12 text-[#303030]">
+        <div className="text-4xl ml-5 font-black">{title}</div>
+        <div className="font-sans ml-5 flex text-left text-xl">{location}</div>
+        <div className="font-sans ml-5 mb-2 mt-3 mr-2 text-lg">
           {description}
         </div>
       </div>
