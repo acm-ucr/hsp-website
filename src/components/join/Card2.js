be9be756585c7092bc55642cbd582344781a5242
Raidah Fairooz <rfair008@ucr.edu>
2024-02-18 00:09:15 -0800

    changed icons

--- src/components/join/Card2.js
+++ src/components/join/Card2.js
@@ -1,15 +1,14 @@
 import React from "react";
-import {
-  IoMdCloseCircleOutline,
-  IoMdRemoveCircleOutline,
-} from "react-icons/io";
-
+import { IoIosCloseCircle } from "react-icons/io";
+import { MdRemoveCircle } from "react-icons/md";
+import { MdExpandCircleDown } from "react-icons/md";
 const Card2 = ({ title, description, icon }) => {
   return (
     <div className="bg-white text-black border border-black rounded-3xl my-5 mx-4">
       <div className="flex items-center text-2xl mx-4 my-2">
-        <IoMdCloseCircleOutline className="mx-1" />
-        <IoMdRemoveCircleOutline className="mx-1" />
+        <IoIosCloseCircle className="mx-1" />
+        <MdRemoveCircle className="mx-1" />
+        <MdExpandCircleDown className="mx-1" />
       </div>
       <div className="border border-black"></div>
       <div className="font-montserrat text-3xl font-extrabold leading-10 p-3">
