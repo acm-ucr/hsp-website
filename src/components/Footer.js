5d25910e4e0b87ebd69b33f82d9a14d6f3d848ee
Vaneesha Singh <vsingh@Vaneeshas-MacBook-Pro.local>
2024-02-21 10:22:38 -0800

    Responsify Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,24 +14,20 @@ import {
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-2 gap-8 text-white bg-black w-full pt-24 pb-4">
+    <div className="grid grid-cols-2 gap-8 text-white bg-black w-full py-4">
       <div className="flex justify-start ">
         <Image src={logo} />
       </div>
       <div className="grid grid-cols-3 divide-x-4 divide-white justify-center items-center ">
-        <div className="font-montserrat h-5/6 grid col-span-2 justify-center items-center ">
-          <div className="text-xl font-semibold flex justify-end">
-            contact us!
-          </div>
-          <div className="text-xl font-semibold flex justify-end">
+        <div className="sm:text-xs md:text-sm lg:text-xl font-montserrat h-5/6 grid col-span-2 justify-center items-center sm:mr-4">
+          <div className="font-semibold flex justify-end">contact us!</div>
+          <div className="font-semibold flex justify-end">
             Winston Chung Hall, 446 N Campus Dr, <br /> Riverside, CA 92507
           </div>
-          <div className="text-xl font-semibold flex justify-end">
-            714-749-5576
-          </div>
+          <div className="font-semibold flex justify-end">714-749-5576</div>
         </div>
-        <div className="grid grid-cols-2 text-5xl text-white h-5/6 hover:curson-pointer ">
-          <div className="flex justify-between flex-col items-center">
+        <div className="grid grid-cols-2 sm:text-xl md:text-3xl lg:text-5xl text-white h-5/6 hover:curson-pointer ">
+          <div className="flex justify-between flex-col items-center sm:px-8">
             <Link
               className="text-white "
               href="https://github.com/acm-ucr/hsp-website/pull/16/files"
