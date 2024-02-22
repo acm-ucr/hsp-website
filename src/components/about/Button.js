6bfc075a16eb8fea30175223a856ed86febcfbbb
Charleen Chen <charleenc485@gmail.com>
2024-02-21 16:06:28 -0800

    fixed see more button

--- src/components/about/Button.js
+++ src/components/about/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-hsp-yellow text-white text-2xl font-bold font-montserrat rounded-[500rem] w-56 h-10 my-4 hover:scale-110">
+    <div className="flex flex-col justify-center items-center bg-hsp-yellow text-white text-2xl font-bold font-montserrat rounded-full w-56 h-10 my-4 hover:scale-110">
       See More
     </div>
   );
