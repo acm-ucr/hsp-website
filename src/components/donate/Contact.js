e41596c52e1aaab0d21c2b914f12cda21aa9007e
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-02-23 16:23:42 -0800

    contact section responsiveness

--- src/components/donate/Contact.js
+++ src/components/donate/Contact.js
@@ -3,26 +3,28 @@ import React from "react";
 const Contact = () => {
   return (
     <div className=" text-center">
-      <div className="text-6xl font-urbanist font-bold ">Contact Us</div>
-      <div className=" font-sans text-xl">
-        <div className="mb-10">highlanderspaceprogram@gmail.com </div>
+      <div className="mt-3 md:mt-5 text-4xl md:text-5xl font-urbanist font-black ">
+        Contact Us
+      </div>
+      <div className=" font-sans text-xs lg:text-xl sm:text-sm">
+        <div className="mb-4">highlanderspaceprogram@gmail.com </div>
 
         <div className="font-extrabold">Javen Dosanjh</div>
 
         <div>President/Chief Engineer</div>
-        <div>jdos001@ucr.edu</div>
+        <div> jdos001@ucr.edu</div>
         <div>(916)-840-8409</div>
 
-        <div className="font-bold mt-10">Jordy Hendra</div>
+        <div className="font-extrabold mt-4">Jordy Hendra</div>
 
         <div>Vice President</div>
         <div>jhend060@ucr.edu</div>
 
-        <div className="mt-10">Winston Chung Hall, 446</div>
+        <div className="mt-4">Winston Chung Hall, 446</div>
 
         <div>N Campus Dr, Riverside</div>
 
-        <div>CA 92507</div>
+        <div className="mb-4 md:mb-5">CA 92507</div>
       </div>
     </div>
   );
