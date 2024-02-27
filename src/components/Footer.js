6773ba9d4c244e8bd19704113f2965fd3a5daf58
Vaneesha Singh <vsingh@Vaneeshas-MacBook-Pro.local>
2024-02-26 21:50:31 -0800

    update mobile responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,20 +14,23 @@ import {
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-2 gap-8 text-white bg-black w-full py-4">
-      <div className="flex justify-start ">
+    <div className="grid grid-cols-2 gap-6 text-white bg-black w-full pt-4 pb-2">
+      <div className="hidden md:block justify-start">
         <Image src={logo} />
       </div>
-      <div className="grid grid-cols-3 divide-x-4 divide-white justify-center items-center ">
-        <div className="sm:text-xs md:text-sm lg:text-xl font-montserrat h-5/6 grid col-span-2 justify-center items-center sm:mr-4">
+      <div className="md:hidden block justify-start">
+        <Image src={logo} />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 md:divide-x-4 divide-white justify-center items-center ">
+        <div className="sm:text-xs md:text-sm lg:text-xl font-montserrat h-5/6 grid col-span-2 justify-center items-center mr-4 mb-3">
           <div className="font-semibold flex justify-end">contact us!</div>
           <div className="font-semibold flex justify-end">
             Winston Chung Hall, 446 N Campus Dr, <br /> Riverside, CA 92507
           </div>
           <div className="font-semibold flex justify-end">714-749-5576</div>
         </div>
-        <div className="grid grid-cols-2 sm:text-xl md:text-3xl lg:text-5xl text-white h-5/6 hover:curson-pointer ">
-          <div className="flex justify-between flex-col items-center sm:px-8">
+        <div className="sm: grid md:grid grid-cols-2 sm:text-3xl lg:text-5xl text-white h-5/6 hover:curson-pointer mr-4">
+          <div className="flex justify-between md:flex-col items-center">
             <Link
               className="text-white "
               href="https://github.com/acm-ucr/hsp-website/pull/16/files"
@@ -41,8 +44,8 @@ const Footer = () => {
               <FaYoutube />
             </Link>
           </div>
-          <div className="flex justify-between flex-col items-start">
-            <Link className="text-white" href="slack-link">
+          <div className="flex justify-between md:flex-col items-center">
+            <Link className="text-white ml-3 md:ml-0" href="slack-link">
               <FaSlack />
             </Link>
             <Link className="text-white" href="email-link">
