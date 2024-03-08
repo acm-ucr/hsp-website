2748466f4203b5de18c2e58749352df30bd4790d
Uma Akundi <139840775+umizoomiexe@users.noreply.github.com>
2024-03-08 15:55:30 -0800

    responsiveness abt us

--- src/components/about/Us.js
+++ src/components/about/Us.js
@@ -1,3 +1,6 @@
+
+
+
 import React from "react";
 import Image from "next/image";
 import image from "@/public/assets/about/aboutUs.svg";
@@ -9,31 +12,35 @@ const Us = () => {
     <div className="text-center flex flex-col justify-center items-center ">
       <div className="mb-40 mt-20 max-h-0 translate-y-12 flex flex-col1">
         <GrBottomCorner
-          size={65}
-          className="origin-top-left translate-x-7 translate-y-5 rotate-90"
+          size={60}
+          // md:size={65}
+          // lg:size={65}
+          className="origin-top-left translate-x-12 translate-y-3 rotate-90"
         />
-        <div className="font-montserrat font-bold text-5xl">ABOUT US</div>
+        <div className="font-montserrat font-bold text-3xl md:text-4xl">
+          ABOUT US
+        </div>
         <GrTopCorner
-          size={65}
-          className="origin-top-right translate-x-5 translate-y-8 rotate-90"
+          size={60}
+          className="origin-top-right translate-x-3 translate-y-8 rotate-90"
         />
       </div>
-      <div className="grid grid-cols-2 justify-center w-full gap-4">
-        <div className="flex flex-col text-center justify-center items-end font-montserrat font-bold ">
-          <div className="text-lg w-9/12">
+      <div className="grid md:grid-cols-2 justify-center gap-4 w-full">
+        <div className="flex flex-col text-center justify-center items-center font-montserrat font-bold">
+          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 sm:w-9/12">
             {" "}
             HSP creates opportunities for its members to collaborate with
             industry professionals and organizations, engage in projects and
-            research that &nbsp; are relevant to the field of aerospace
-            engineering.
+            research that are relevant to the field of aerospace engineering.
           </div>
           <br />
-          <div className="text-lg w-9/12">
+          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 sm:w-9/12">
             HSP regularly conducts meetings and work sessions that build towards
             competitions.
           </div>
         </div>
-        <div className="flex justify-start items-center ">
+        <div className="flex sm:justify-center md:justify-center items-center ">
+          {" "}
           <Image alt="" src={image} />
         </div>
       </div>
