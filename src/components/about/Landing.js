af0d690257652067c5e00e6d044692afd8856f42
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-03-10 23:51:16 -0700

    landing page redo

--- src/components/about/Landing.js
+++ src/components/about/Landing.js
@@ -1,22 +1,15 @@
 import React from "react";
 import Sky from "@/public/assets/about/sky.png";
 import Image from "next/image";
-import rocket from "@/public/assets/about/rocket.svg";
 import comets from "@/public/assets/about/comets.svg";
 import { FaChevronDown } from "react-icons/fa";
 const Landing = () => {
   return (
-    <div className="h-screen w-screen bg-black ">
-      <Image src={Sky} alt="sky" className="bg-transparent absolute" />
-      <Image src={rocket} className="absolute right-0 z-0" />
-      <div
-        className="w-10/12 h-1/2 bg-white justify-start flex items-center"
-        style={{
-          clipPath: "polygon(0 0, 100% -20%, 70% 100%, 0% 100%)",
-        }}
-      >
+    <div className="h-screen w-screen bg-transparent">
+      <Image src={Sky} alt="sky" className="bg-black top-0.5 absolute" />
+      <div className="w-screen h-96 bg-white flex justify-center items-center mt-10 relative">
         <Image src={comets} className="" />
-        <div classname="flex flex-col ">
+        <div classname="flex flex-col items-center ">
           <div className="text-3xl text-black font-extralight font-montserrat my-3">
             Welcome to:
           </div>
@@ -29,9 +22,10 @@ const Landing = () => {
             Aerospace Engineering at UCR
           </div>
         </div>
+        <Image src={comets} className="rotate-90" />
       </div>
-      <div className=" absolute flex justify-center items-center w-5/12 left-60 mt-20 flex-col">
-        <div className="text-center w-9/12 font-montserrat text-xl font-black text-white">
+      <div className=" absolute flex justify-center items-center w-screen mt-32 flex-col">
+        <div className="text-center w-4/12 font-montserrat text-xl font-black text-white">
           promoting and advancing the study and practice of aerospace
           engineering by providing an outlet for members to explore, develop and
           enhance their technical skills.
