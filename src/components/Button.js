dcfee0d32b5215cb9e921b6bda397c5ad0e2c5db
Raidah Fairooz <rfair008@ucr.edu>
2024-03-15 15:54:13 -0700

    done

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <div className="bg-hsp-yellow text-black text-xl md:text-2xl font-bold font-montserrat flex justify-center items-center rounded-3xl min-w-fit px-4 min-h-fit py-1 hover:scale-110 hover:!text-white hover:!bg-hsp-green">
+    <div className="bg-hsp-yellow text-black text-xl md:text-2xl font-bold font-montserrat flex justify-center items-center rounded-3xl min-w-fit px-4 min-h-fit py-1  hover:!text-white hover:!bg-hsp-green">
       join now
     </div>
   );
