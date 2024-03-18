221edbd462456fecab2ac8b7598d091a8a59bc76
Raidah Fairooz <rfair008@ucr.edu>
2024-03-18 15:16:54 -0700

    small fixes

--- src/components/about/Us.js
+++ src/components/about/Us.js
@@ -23,22 +23,22 @@ const Us = () => {
         />
       </div>
       <div className="grid md:grid-cols-2 justify-center gap-4 w-full">
-        <div className="flex flex-col text-center justify-center items-center font-montserrat font-bold">
-          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 sm:w-9/12">
+        <div className="flex flex-col text-center justify-center items-center lg:items-end font-montserrat font-bold">
+          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 w-10/12">
             {" "}
             HSP creates opportunities for its members to collaborate with
             industry professionals and organizations, engage in projects and
             research that are relevant to the field of aerospace engineering.
           </div>
           <br />
-          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 sm:w-9/12">
+          <div className="md:text-lg sm:text-md lg:7/12 md:w-9/12 w-10/12">
             HSP regularly conducts meetings and work sessions that build towards
             competitions.
           </div>
         </div>
-        <div className="flex sm:justify-center md:justify-center items-center ">
+        <div className="flex justify-center lg:justify-start items-center">
           {" "}
-          <Image alt="" src={image} />
+          <Image className="w-11/12 lg:w-10/12" alt="" src={image} />
         </div>
       </div>
     </div>
