17c4fc7ac06de0e4092426ec67b4210542ac9a6b
Raidah Fairooz <rfair008@ucr.edu>
2024-03-18 14:32:37 -0700

    small fixes

--- src/components/projects/poseidon/Timeline.js
+++ src/components/projects/poseidon/Timeline.js
@@ -3,32 +3,31 @@ import texts from "@/data/Poseidon/Timeline";
 
 const Timeline = () => {
   return (
-    <div className="flex flex-col items-center my-10 gap-3">
-      <div className="flex text-left w-9/12 text-3xl font-extrabold font-montserrat pb-4">
+    <div className="flex flex-col items-center my-10">
+      <div className="flex lg:justify-start justify-center lg:w-9/12 text-3xl font-extrabold font-montserrat pb-4">
         Timeline
       </div>
-      <div></div>
       {texts.map((event, index) => (
-        <div key={index} className="flex w-9/12 items-start ">
+        <div key={index} className="flex w-11/12 lg:w-9/12 items-start ">
           <div className="flex items-center justify-center">
             <div>
               <div className="flex flex-col items-center">
-                <div className=" bg-blue-500 rounded-full w-10 h-10 -mt-8"></div>
+                <div className=" bg-blue-500 rounded-full w-10 h-10"></div>
 
                 {index !== texts.length - 1 && (
-                  <div className="w-1 bg-black sm:h-13 h-96"></div>
+                  <div className="bg-black relative h-20 w-1"></div>
                 )}
               </div>
             </div>
           </div>
 
-          <div className="grid md:grid-cols-2 font-sans gap-3 -mt-10 ">
-            <div className="text-2xl flex justify-center items-start flex-col pl-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 font-sans ">
+            <div className="text-lg lg:text-2xl flex justify-center items-start ">
               {event.data}
-
-              <span className="block md:hidden mt-4">{event.para}</span>
             </div>
-            <div className="hidden md:block text-xl">{event.para}</div>
+            <div className="text-sm lg:text-xl text-left flex justify-center items-center">
+              {event.para}
+            </div>
           </div>
         </div>
       ))}
