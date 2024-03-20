c0658d9c9164217df0a4bc36418458ee0e8183e3
kavinphab <kavinphab@gmail.com>
2024-03-20 13:08:59 -0700

    formatting issues

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,27 +7,20 @@ const error = () => {
     <div className=" flex flex-col my-20 items-center gap-8 justify-center">
       <div className="flex flex-col items-center gap-2 text-center">
         <br />
-        <div className="text-2xl lg:text-4xl">
-          {" "}
-          404 Error: Page not found 
-        </div>
+        <div className="text-2xl lg:text-4xl"> 404 Error: Page not found</div>
         <br />
         <div className="text-xl lg:text-2xl ">
-          This is uncharted territory. Please return to the 
-          <Link
-        href="/"
-        className="px-2 underline hover:text-blue-700"
-      >
-        home
-      </Link>page.
+          This is uncharted territory. Please return to the
+          <Link href="/" className="px-2 underline hover:text-blue-700">
+            home
+          </Link>
+          page.
         </div>
-        
+
         <div className="items-center justify-center w-1/2 h-1/2">
           <Image src={bear} alt="" />
         </div>
       </div>
-      
-      
     </div>
   );
 };
