9c6ad8dc90586be379349deef767b9d3c9b7a14a
Raidah Fairooz <rfair008@ucr.edu>
2024-03-31 05:58:04 -0700

    added sponsor link, changed favicon, fixed bugs

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -19,7 +19,7 @@ const page = () => {
     <div className="flex justify-center w-screen flex-col items-center font-sans ">
       <PageTitle title="SPONSORS" />
       <div className=" w-10/12 lg:w-7/12 justify-center">
-        <div className="grid md:grid-cols-3 my-8 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 my-8 lg:gap-4 ">
           <div className="hidden md:flex justify-center">
             <Image src={Image1} alt="building rocket" />
           </div>
@@ -27,10 +27,9 @@ const page = () => {
           <div className=" justify-start items-center grid col-span-2">
             <Description key={0} title={first.heading} texts={first.para} />
           </div>
-        </div>
-
-        <div className="block md:hidden itmes-center justify-center">
-          <Image src={Image1} alt="" />
+          <div className="flex md:hidden itmes-center  justify-center">
+            <Image src={Image1} alt="" />
+          </div>
         </div>
 
         <div>
@@ -43,11 +42,11 @@ const page = () => {
           ))}
         </div>
 
-        <div className="grid md:grid-cols-3 ">
-          <div className=" grid md:col-span-2">
+        <div className="grid grid-cols-1 md:grid-cols-3 ">
+          <div className=" grid grid-cols-1 md:col-span-2">
             <Description key={0} title={last.heading} texts={last.para} />
           </div>
-          <div className="block md:flex justify-center items-center">
+          <div className="flex justify-center items-center">
             <Image src={Image2} alt="college ranking" />
           </div>
         </div>
--- src/components/donate/Sponsors.js
+++ src/components/donate/Sponsors.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
-
+import Link from "next/link";
 const Sponsors = ({ logos }) => {
   return (
     <div className="mt-12 text-center flex flex-col justify-center items-center h-full">
       <div className="mb-2 text-3xl lg:text-6xl font-extrabold font-urbanist">
         Sponsors
       </div>
-      <div className="justify-center font-sans text-lg w-full">
+      <div className="justify-center font-sans text-base lg:text-lg w-10/12 lg:w-full">
         We are excited to share our progress and our future goals. Thank you for
         your support.
       </div>
       <div className="my-12 flex flex-wrap justify-start">
         {logos.map((logos, index) => (
-          <div key={index} className="flex flex-wrap m-2">
-            <Image src={logos.img} alt={""} width={250} height={200} />
+          <div
+            key={index}
+            className="flex flex-wrap m-2 hover:cursor-pointer hover:scale-105"
+          >
+            <Link href={logos.link}>
+              <Image src={logos.img} alt={""} width={250} height={200} />
+            </Link>
           </div>
         ))}
       </div>
